c1d86ec6b9dc2ecc6f3ed6e8a08638be35ddb873
Ryan Voots <simcop2387@simcop2387.info>
2016-09-18 19:26:45 -0700

    Make the example accept an adapter description as a command line option.  From Leonerd.

--- examples/sweep.pl
+++ examples/sweep.pl
@@ -8,11 +8,17 @@ use lib './lib';
 use Device::Chip::PCA9685;
 use Device::Chip::Adapter;
 
-my $adapter = Device::Chip::Adapter->new_from_description("LinuxKernel");
+use Getopt::Long;
+
+GetOptions(
+    # This is the i2c bus on an RPI 2 B+
+    'adapter|A=s' => \(my $ADAPTER = "LinuxKernel:bus=/dev/i2c-1"),
+) or exit 1;
+
+my $adapter = Device::Chip::Adapter->new_from_description($ADAPTER);
 
 my $chip = Device::Chip::PCA9685->new();
-# This is the i2c bus on an RPI 2 B+
-$chip->mount($adapter, bus => '/dev/i2c-1')->get;
+$chip->mount($adapter)->get;
 
 $chip->enable();
 $chip->set_frequency(400); # 400 Hz
